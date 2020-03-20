e56ef17595abfda03370625beb810f5dd726c947
Naughty1905 <minion1905@gmail.com>
2020-03-20 22:23:40 +0300

    repush without idea

--- array-test.js
+++ array-test.js
@@ -1,3 +1,7 @@
+// test for finding the sum of element indexes ,
+// where the sum of all digits before the element equals
+// the sum of all digits after the element
+
 function arraySumIndex(array) {
     let sumOfArray = array.reduce((res, el) => res + el, 0);
     let iterateSum = 0;
