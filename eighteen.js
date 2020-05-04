3332457f8e9b86a08693f09039806dee0ea2099b
Naughty1905 <minion1905@gmail.com>
2020-05-04 21:16:51 +0300

    task number eighteen

---
+++ eighteen.js
@@ -0,0 +1,7 @@
+function solution(input, markers) {
+    return input.split('\n').map(
+        line => markers.reduce(
+            (line, marker) => line.split(marker)[0].trim(), line
+        )
+    ).join('\n')
+}l
