4df8d4b7ca1ce7fa6962e4f047cf5ab07fb35b7b
Naughty1905 <minion1905@gmail.com>
2020-05-07 22:30:13 +0300

    task number nineteen

---
+++ nineteen.js
@@ -0,0 +1,15 @@
+function pathFinder(maze) {
+    function solve(pos) {
+        if ( pos===width*width-1 ) return true;
+        if ( maze[pos]!=="." ) return false;
+        maze = maze.slice(0,pos)+"#"+maze.slice(pos+1);
+        return (pos+1)%width!==0 && solve(pos+1) ||
+            solve(pos+width) ||
+            pos%width!==0 && solve(pos-1) ||
+            pos>width && solve(pos-width)
+            ;
+    }
+    const width = maze.indexOf("\n");
+    maze = maze.replace(/\n/g,"");
+    return solve(0);
+}
